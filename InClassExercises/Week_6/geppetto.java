8083185a7dcfb4490b6a69c526a03f4d61405680
Mitchel Carson <mitchel.carson@gmail.com>
2025-02-23 10:04:33 -0500

    geppetto.java in progress.

--- InClassExercises/Week_6/geppetto.java
+++ InClassExercises/Week_6/geppetto.java
@@ -1,57 +1,48 @@
-import java.util.*;
-
-import javax.management.loading.MLet;
+import java.util.Scanner;
 
 public class geppetto {
-
     public static void main(String[] args) {
-        // ingedients 1 to N
-        // some ingredients cannot mix
-        // M pairs of ingredients that cannot mix
-        // How many different pizzas can be made
-        // Pizzas at ingredient are considered different of index i that is different
-        // from index j
-
         Scanner sc = new Scanner(System.in);
-        // first line contains N and M
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        // each of the next M lines contains two integers A and B. 1<=A,B<=N. A and B
-        // cannot mix. A and B can occur multiple times
-        List<List<Integer>> cannotMix = new ArrayList<>();
+
+        int N = sc.nextInt();  // Number of ingredients
+        int M = sc.nextInt();  // Number of conflicting pairs
+
+        // Store conflict pairs in a 2D array
+        int[][] conflicts = new int[M][2];
         for (int i = 0; i < M; i++) {
-            List<Integer> pair = new ArrayList<>();
-            pair.add(sc.nextInt());
-            pair.add(sc.nextInt());
-            cannotMix.add(pair);
-        }
-        sc.close();
-
-        // output the number of different pizzas that can be made
-
-        // create a list of ingredients
-        List<Integer> ingredients = new ArrayList<>();
-
-        for (int i = 1; i <= N; i++) {
-            ingredients.add(i);
+            conflicts[i][0] = sc.nextInt();
+            conflicts[i][1] = sc.nextInt();
         }
 
-        // calcuate the number of pizzas that can be made
-       int count = N * (N - 1) / 2;
-        for (int i = 0; i < ingredients.size(); i++) {
-            for (int j = i + 1; j < ingredients.size(); j++) {
-                List<Integer> pizza = new ArrayList<>();
-                pizza.add(ingredients.get(i));
-                pizza.add(ingredients.get(j));
-                if (!cannotMix.contains(pizza)) {
-                    count--;
+        int count = 0;
+        // We will iterate through all possible subsets of the N ingredients
+        // mask goes from 0 to 2^N - 1
+        int totalSubsets = 1 << N;
+
+        for (int mask = 0; mask < totalSubsets; mask++) {
+            boolean valid = true;
+
+            // Check each conflict pair
+            for (int i = 0; i < M; i++) {
+                // Convert 1-based ingredient index to 0-based bit position
+                int a = conflicts[i][0] - 1;
+                int b = conflicts[i][1] - 1;
+
+                // Check if both ingredients a and b are in the subset (mask)
+                if (((mask >> a) & 1) == 1 && ((mask >> b) & 1) == 1) {
+                    valid = false;
+                    break;
                 }
             }
+
+            if (valid) {
+                count++;
+            }
         }
-        if(count > 0) {
-            System.out.println(count+1);
-        } else {
-            System.out.println(0);
-        }
+
+        // Print the number of valid subsets
+        System.out.println(count);
+
+        sc.close();
     }
 }
--- InClassExercises/Week_6/zagrade.java
+++ InClassExercises/Week_6/zagrade.java
@@ -1,6 +1,8 @@
 public class zagrade {
 
     public static void main(String[] args) {
-        
+        //produce the powerset
+        //accumulate the number of open and close brackets and increment a variable
+        //we can remove the given input and just return the other values
     }
 }
