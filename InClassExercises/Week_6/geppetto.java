ab29584fc4e524405ac38062ec5c397042723ba3
Mitchel Carson <mitchel.carson@gmail.com>
2025-02-26 20:11:00 -0500

    zagrade.java geppetto.java complete.

--- InClassExercises/Week_6/geppetto.java
+++ InClassExercises/Week_6/geppetto.java
@@ -4,10 +4,11 @@ public class geppetto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();  // Number of ingredients
-        int M = sc.nextInt();  // Number of conflicting pairs
+        // Number of ingredients
+        int N = sc.nextInt();  
+        // Number of conflicting pairs
+        int M = sc.nextInt();  
 
-        // Store conflict pairs in a 2D array
         int[][] conflicts = new int[M][2];
         for (int i = 0; i < M; i++) {
             conflicts[i][0] = sc.nextInt();
@@ -15,34 +16,28 @@ public class geppetto {
         }
 
         int count = 0;
-        // We will iterate through all possible subsets of the N ingredients
-        // mask goes from 0 to 2^N - 1
-        int totalSubsets = 1 << N;
+        // 2^N possible subsets
+        int totalSubsets = 1 << N;  
 
         for (int mask = 0; mask < totalSubsets; mask++) {
             boolean valid = true;
-
-            // Check each conflict pair
             for (int i = 0; i < M; i++) {
-                // Convert 1-based ingredient index to 0-based bit position
+                // Ingredients are 1 based in the problem; convert to 0 based
                 int a = conflicts[i][0] - 1;
                 int b = conflicts[i][1] - 1;
 
-                // Check if both ingredients a and b are in the subset (mask)
+                // Check if both ingredients a and b are chosen in this subset
                 if (((mask >> a) & 1) == 1 && ((mask >> b) & 1) == 1) {
                     valid = false;
                     break;
                 }
             }
-
             if (valid) {
                 count++;
             }
         }
 
-        // Print the number of valid subsets
         System.out.println(count);
-
         sc.close();
     }
 }
--- InClassExercises/Week_6/zagrade.java
+++ InClassExercises/Week_6/zagrade.java
@@ -1,8 +1,61 @@
-public class zagrade {
+import java.util.*;
 
+public class zagrade {
     public static void main(String[] args) {
-        //produce the powerset
-        //accumulate the number of open and close brackets and increment a variable
-        //we can remove the given input and just return the other values
+        Scanner sc = new Scanner(System.in);
+        String expr = sc.nextLine();  // read the entire expression
+        sc.close();
+
+        //  Identify all matching parentheses pairs using a stack
+        Stack<Integer> stack = new Stack<>();
+        List<int[]> pairs = new ArrayList<>();
+        for (int i = 0; i < expr.length(); i++) {
+            char c = expr.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                int openIndex = stack.pop();
+                pairs.add(new int[] {openIndex, i});
+            }
+        }
+
+        // collect all resulting expressions in a sorted set
+        Set<String> results = new TreeSet<>();
+
+        // number of parenthesis pairs
+        int n = pairs.size();
+        // 2^n possible ways to remove/keep pairs
+        int totalSubsets = 1 << n;       
+
+        // For each subset, remove the chosen pairs
+        for (int mask = 0; mask < totalSubsets; mask++) {
+            // Mark positions of parentheses to remove
+            boolean[] removed = new boolean[expr.length()];
+            for (int i = 0; i < n; i++) {
+                if (((mask >> i) & 1) == 1) {
+                    int openIdx = pairs.get(i)[0];
+                    int closeIdx = pairs.get(i)[1];
+                    removed[openIdx] = true;
+                    removed[closeIdx] = true;
+                }
+            }
+
+            // Build the new expression
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < expr.length(); i++) {
+                if (!removed[i]) {
+                    sb.append(expr.charAt(i));
+                }
+            }
+
+            results.add(sb.toString());
+        }
+
+        // Print all distinct expressions in lex order
+        // do not print the given expression
+        results.remove(expr);
+        for (String s : results) {
+            System.out.println(s);
+        }
     }
 }
