51ac0de7cf8fdf6333821db80a7f70cbb0e9ae89
Mitchel Carson <mitchel.carson@gmail.com>
2025-03-12 15:09:20 -0400

    header and problem describption added.

---
+++ Katiss_Extra/falling.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class falling {
+    /*
+     * Mitchel Carson
+     * Wed MAR 21 2025
+     * Boone, NC
+     * 
+     * Falling Mugs Problem
+     * 
+     * 
+     * Susan is makiing high-speed videos of falling coffee mugs. when alayzeing the
+     * videos she wants to know how big the mugs are, but unfortunately they all go
+     * destroyed during filming. susan knows some physics though, she has figured
+     * out how far the moved between different video frames. the camera was filmeing
+     * at a speed of 70 frames per second, which means that a frame n, counted from
+     * when the mug was released, the number of millimeters it has moved is d = n^2.
+     * The countingn of the frames starts at 0. Susan thinks a certain mug is D
+     * militmeters high, but to verify theis she needs to find two frames between
+     * which the mug has moved exactly this disntace,. Can you help her do this?
+     * 
+     * Input: The input file contains the single positive intefer D <= 200000, the
+     * distnace to be measured.
+     * 
+     * Output: Output two non-negative integers n1 and n2, the numbers of the frames
+     * that Susan whould compare. They should fulfill n^2(2) - n^2(1) = D. If no two
+     * such integers exist, instead outut "impossible".
+     */
+    public static void main(String[] args) {
+
+    }
+}
